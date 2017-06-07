60a3fc3a20dd0c74f264ab4cdf29c57021675966
James <jayms.portune@gmail.com>
2017-06-06 23:22:46 -0700

    Push to save work, need to pass array into function to complete

--- app.js
+++ app.js
@@ -57,8 +57,13 @@ Write a function called sumArray() that takes in an array of numbers as its argu
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
-// Write your code here
-testArray = [2,3,4];
+function sumArray(sum1, sum2, sum3){
+  var sumNumbersInArray = sum1 + sum2 + sum3;
+  var solutionToSumNums = [sumNumbersInArray, sum1 + "," + sum2 + "," + sum3 + " was passed in as an array of numbers, and " + sumNumbersInArray + " is their sum."];
+  return solutionToSumNums;
+}
+
+var testArray = [2,3,4];
 
 // Here is the test for sumArray(); uncomment it to run it
 testSumArray(testArray);
